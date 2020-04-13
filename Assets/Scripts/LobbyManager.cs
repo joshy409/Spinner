97343be3d63c5ac79bf89e4b9efecc697b466f35
Yang Joshua <ybd1992@hotmail.com>
2020-04-13 15:14:46 -0700

    Scene Loading single script

--- Assets/Scripts/LobbyManager.cs
+++ Assets/Scripts/LobbyManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
+using UnityEngine.SceneManagement;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
@@ -59,6 +60,12 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         {
             Debug.Log("Player name is invalid or empty!");
         }
+    }
+
+    public void OnQuickMatchButtonClicked()
+    {
+        //SceneManager.LoadScene("Scene_Loading");
+        SceneLoader.Instance.LoadScene("Scene_PlayerSelection");
     }
     #endregion
 
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : Singleton<SceneLoader>
+{
+    private string sceneNameToBeLoaded;
+
+    public void LoadScene(string sceneName)
+    {
+        sceneNameToBeLoaded = sceneName;
+
+        StartCoroutine(InitializeSceneLoading());
+    }
+
+    IEnumerator InitializeSceneLoading()
+    {
+        yield return SceneManager.LoadSceneAsync("Scene_Loading");
+
+        StartCoroutine(LoadAcutalScene());
+
+    }
+
+    IEnumerator LoadAcutalScene()
+    {
+       var asyncSceneLoading = SceneManager.LoadSceneAsync(sceneNameToBeLoaded);
+
+        //this stops the scen efrom displaying when it is still loading...
+        asyncSceneLoading.allowSceneActivation = false;
+
+        while(!asyncSceneLoading.isDone)
+        {
+            Debug.Log(asyncSceneLoading.progress);
+            if(asyncSceneLoading.progress >= .0f)
+            {
+                //show the scene
+                asyncSceneLoading.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
