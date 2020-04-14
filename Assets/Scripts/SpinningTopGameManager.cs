a36207299e9d429e923b76dc52f9b056fbe93688
Yang Joshua <ybd1992@hotmail.com>
2020-04-13 17:22:16 -0700

    Match making with Photon

---
+++ Assets/Scripts/SpinningTopGameManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class SpinningTopGameManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region UI Callback Methods
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+    #endregion
+
+    #region Photon Callback Methods
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(message);
+        CreateAndJoinRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(PhotonNetwork.NickName + " Joined to " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log(newPlayer.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+    #endregion
+
+    #region Private Methods
+    private void CreateAndJoinRoom()
+    {
+        string randomRoomName = "Room" + Random.Range(0, 1000);
+
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        //creating rooms
+        PhotonNetwork.CreateRoom(randomRoomName, roomOptions);
+    }
+    #endregion
+}
